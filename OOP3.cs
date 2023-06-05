6fc3b1e734343e640593917f15506d751fbb2843
heligayakwad <128084580+heligayakwad@users.noreply.github.com>
2023-06-04 23:51:08 -0400

    Add files via upload

--- OOP3.cs
+++ OOP3.cs
@@ -1,22 +1,36 @@
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// Represents a pack that can hold inventory items with specific constraints.
+/// </summary>
 public class Pack
 {
-  private List<InventoryItem> items;  // List to store the inventory items in the pack
-    private int maxCount;               // Maximum number of items allowed in the pack
-    private int maxVolume;              // Maximum volume allowed in the pack
-    private int maxWeight;              // Maximum weight allowed in the pack
+    private List<InventoryItem> items;
+    private int maxCount;
+    private int maxVolume;
+    private int maxWeight;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Pack"/> class with the specified constraints.
+    /// </summary>
+    /// <param name="maxCount">The maximum number of items allowed in the pack.</param>
+    /// <param name="maxVolume">The maximum volume allowed in the pack.</param>
+    /// <param name="maxWeight">The maximum weight allowed in the pack.</param>
     public Pack(int maxCount, int maxVolume, int maxWeight)
     {
         this.maxCount = maxCount;
         this.maxVolume = maxVolume;
         this.maxWeight = maxWeight;
 
-        items = new List<InventoryItem>(); // Initialize the list of items
+        items = new List<InventoryItem>();
     }
-// Adds an inventory item to the pack if it satisfies the constraints
+
+    /// <summary>
+    /// Adds an inventory item to the pack if it satisfies the constraints.
+    /// </summary>
+    /// <param name="item">The inventory item to add.</param>
+    /// <returns><c>true</c> if the item was successfully added; otherwise, <c>false</c>.</returns>
     public bool Add(InventoryItem item)
     {
         if (items.Count < maxCount && GetTotalVolume() + item.Volume <= maxVolume && GetTotalWeight() + item.Weight <= maxWeight)
@@ -29,13 +43,21 @@ public class Pack
             return false;
         }
     }
-// Removes an inventory item from the pack
+
+    /// <summary>
+    /// Removes an inventory item from the pack.
+    /// </summary>
+    /// <param name="item">The inventory item to remove.</param>
     public void Remove(InventoryItem item)
     {
         items.Remove(item);
     }
 
-    public string ToString()
+    /// <summary>
+    /// Returns a string representation of the pack and its contents.
+    /// </summary>
+    /// <returns>A string representation of the pack.</returns>
+    public override string ToString()
     {
         string result = $"Pack contains {items.Count} items with total weight {GetTotalWeight()} and volume {GetTotalVolume()}:\n";
         
@@ -46,73 +68,45 @@ public class Pack
 
         return result;
     }
-// Calculates the total volume of all items in the pack
+
+    /// <summary>
+    /// Calculates the total volume of all items in the pack.
+    /// </summary>
+    /// <returns>The total volume of all items in the pack.</returns>
     private float GetTotalVolume()
     {
         float totalVolume = 0f;
-
         foreach (var item in items)
         {
             totalVolume += item.Volume;
         }
-
         return totalVolume;
-     }
+    }
 
-     private float GetTotalWeight()
-     {
-         float totalWeight = 0f;
-
-         foreach (var item in items)
-         {
-             totalWeight += item.Weight;
-         }
-
-         return totalWeight;
-     }
-}
-// Abstract base class representing an inventory item
-public abstract class InventoryItem
-{
-    public string Name { get; set; } // Name of the inventory item
-    public float Weight { get; set; } // Weight of the inventory item in kg
-    public float Volume { get; set; } // Volume of the inventory item in liters
-
-
-    public InventoryItem(string name, float weight, float volume)
+    /// <summary>
+    /// Calculates the total weight of all items in the pack.
+    /// </summary>
+    /// <returns>The total weight of all items in the pack.</returns>
+    private float GetTotalWeight()
     {
-        Name = name;
-        Weight = weight;
-        Volume = volume;
+        float totalWeight = 0f;
+        foreach (var item in items)
+        {
+            totalWeight += item.Weight;
+        }
+        return totalWeight;
     }
 }
-// Concrete classes representing specific inventory items
-public class Arrow : InventoryItem
-{
-    public Arrow() : base("Arrow", 0.1f, 0.05f) {}
-}
 
-public class Bow : InventoryItem
+/// <summary>
+/// Represents an inventory item.
+/// </summary>
+public abstract class InventoryItem
 {
-    public Bow() : base("Bow", 1f, 4f) {}
-}
+    /// <summary>
+    /// Gets or sets the name of the inventory item.
+    /// </summary>
+    public string Name { get; set; }
 
-public class Rope : InventoryItem
-{
-    public Rope() : base("Rope", 1f, 1.5f) {}
-}
-
-public class Water : InventoryItem
-{
-    public Water() : base("Water", 2f, 3f) {}
-}
-
-public class Food : InventoryItem
-{
-    public Food() : base("Food", 1f, 0.5f) {}
-}
-
-public class Sword : InventoryItem
-{
-   public Sword() : base("Sword", 5f, 3f) {}
-}
+    /// <summary>
+   
